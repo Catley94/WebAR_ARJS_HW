7520faa69510d9675d1a5a7d5d90b501e3f17f1a
positive-season <scp94373@gmail.com>
2023-09-30 00:14:24 +0100

    Added two other objects that spawn near by

--- basic.js
+++ basic.js
@@ -6,20 +6,50 @@ window.onload = () => {
     el.addEventListener("gps-camera-update-position", e => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-entity");
-            entity.setAttribute("scale", {
+            // Add a sphere to the north of the initial GPS position
+            const sphere = document.createElement("a-entity");
+            sphere.setAttribute("scale", {
                 x: 20,
                 y: 20,
                 z: 20
             });
-            entity.setAttribute('geometry', 'primitive: sphere');
-            entity.setAttribute('material', { color: 'red' } );
-            entity.setAttribute('gps-new-entity-place', {
+            sphere.setAttribute('geometry', 'primitive: sphere');
+            sphere.setAttribute('material', { color: 'red' } );
+            sphere.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             });
-            document.querySelector("a-scene").appendChild(entity);
+
+            const box = document.createElement("a-entity");
+            box.setAttribute("scale", {
+                x: 20,
+                y: 20,
+                z: 20
+            });
+            box.setAttribute('geometry', 'primitive: box');
+            box.setAttribute('material', { color: 'red' } );
+            box.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.002,
+                longitude: e.detail.position.longitude
+            });
+
+            const cylinder = document.createElement("a-entity");
+            cylinder.setAttribute("scale", {
+                x: 20,
+                y: 20,
+                z: 20
+            });
+            cylinder.setAttribute('geometry', 'primitive: box');
+            cylinder.setAttribute('material', { color: 'red' } );
+            cylinder.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude - 0.002,
+                longitude: e.detail.position.longitude
+            });
+
+
+            document.querySelector("a-scene").appendChild(sphere);
+            document.querySelector("a-scene").appendChild(box);
+            document.querySelector("a-scene").appendChild(cylinder);
         }
         testEntityAdded = true;
     });
