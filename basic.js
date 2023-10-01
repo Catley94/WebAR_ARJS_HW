a838756e58f66d84087dfb79d14b48d8f20eb82e
positive-season <scp94373@gmail.com>
2023-10-01 22:52:34 +0100

    Testing creating box in position of updated coords

--- basic.js
+++ basic.js
@@ -9,6 +9,24 @@ window.onload = () => {
 
 
     el.addEventListener("gps-camera-update-position", async(e) => {
-
+        const compoundEntity = document.createElement("a-entity");
+        compoundEntity.setAttribute('gps-new-entity-place', {
+            latitude: e.detail.position.latitude,
+            longitude: e.detail.position.longitude - 0.0001,
+        });
+        const box = document.createElement("a-box");
+        box.setAttribute("scale", {
+            x: 20,
+            y: 20,
+            z: 20
+        });
+        box.setAttribute('material', { color: 'red' } );
+        box.setAttribute("position", {
+            x : 0,
+            y : 20,
+            z: 0
+        } );
+        compoundEntity.appendChild(box);
+        document.querySelector("a-scene").appendChild(compoundEntity);
     });
 };
