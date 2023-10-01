f6b7f465e42971f5c9b4e73e60ce9c9213398119
positive-season <scp94373@gmail.com>
2023-10-01 22:42:41 +0100

    Revert "Set up of event listeners."
    This reverts commit 771e5803f1f96a9fa86e1e3924c044b78dee540e.

--- basic.js
+++ basic.js
@@ -2,12 +2,6 @@ window.onload = () => {
     const headset = document.querySelector("#vrheadset_loaded");
     const el = document.querySelector("[gps-new-camera]");
 
-    headset.addEventListener("click", (e) => {
-        console.log("Clicked");
-        alert("VR Clicked");
-    })
-
-
     el.addEventListener("gps-camera-update-position", async(e) => {
 
     });
