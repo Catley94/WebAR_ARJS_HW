71d0e908dc007060234b7962095362e845916f24
positive-season <scp94373@gmail.com>
2023-10-01 23:04:31 +0100

    Change headset lon and lat to where camera is

--- basic.js
+++ basic.js
@@ -10,10 +10,10 @@ window.onload = () => {
         alert("VR Clicked");
     })
 
-
     el.addEventListener("gps-camera-update-position", async(e) => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            headset.setAttribute("gps-new-entity-place", `latitude: ${e.detail.position.latitude + 0.001}; longitude: ${e.detail.position.longitude}`);
             // Add a box to the north of the initial GPS position
             const entity = document.createElement("a-box");
             entity.setAttribute("scale", {
